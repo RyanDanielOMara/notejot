520ac8447ac1fa44ea65e469bac829fa75cd6232
Ryan O'Mara <ryandanielomara@gmail.com>
2018-01-08 12:18:33 -0500

    Add handlebars, middleware, basic routing.
    Add handlebars to our application, create main layout and about, index
    views.  Add middleware for handlebars.  Add routes for index and about
    pages.

--- app.js
+++ app.js
@@ -5,18 +5,41 @@
 
 const express = require('express');
 const app     = express();
+const exphbs  = require('express-handlebars');
 const port    = 5000;
 
-// Index route
-app.get('/', (req, res) => {
-    res.send('Index');
-});
-
-// About route
-app.get('/about', (req, res) => {
-    res.send('About')
-})
+init_middleware()
+init_routes()
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+/**
+ * Initialize middleware
+ * Handlebars middleware for template rendering
+ */
+function init_middleware(){
+    // Handlebars middleware
+    app.engine('handlebars', exphbs({
+        defaultLayout: 'main'
+    }));
+    app.set('view engine', 'handlebars');
+}
+
+/**
+ * Initialize routes
+ * Index
+ * About
+ */
+function init_routes(){
+    // Index route
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    // About route
+    app.get('/about', (req, res) => {
+        res.render('about')
+    })
+}
